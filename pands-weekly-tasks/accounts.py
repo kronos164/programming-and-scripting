9ecee02fe2232b5658ce108c1da0ca0de3e10054
Cainã Oliveira <caina.boliveira@outlook.com>
2025-02-11 19:13:41 +0000

    week 3 task - accounts.py

---
+++ pands-weekly-tasks/accounts.py
@@ -0,0 +1,9 @@
+
+# Read in a user's account number
+account_n = input("Enter your account number: ")
+
+# Mask the account number
+masked_account_n = 'X' * (len(account_n) - 4) + account_n[-4:]
+
+# Print the masked account number
+print("Your masked account number is: " + masked_account_n)
