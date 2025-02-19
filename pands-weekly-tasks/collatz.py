a086c373cbcdb4c2259730d08b86e12cfad3b38d
Cainã Oliveira <caina.boliveira@outlook.com>
2025-02-19 20:15:41 +0000

    Assingment week 4, collatz.py

---
+++ pands-weekly-tasks/collatz.py
@@ -0,0 +1,11 @@
+def main():
+    n = int(input("Please enter a positive integer: "))
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        print(n)
+
+if __name__ == "__main__":
+    main()
