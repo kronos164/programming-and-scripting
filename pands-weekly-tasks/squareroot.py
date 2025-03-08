f489db04389658e8b6d367f1ff6910f1bb14174e
Cainã Oliveira <caina.boliveira@outlook.com>
2025-03-08 23:06:57 +0000

    Add square root calculation to the assingment 6, squreroot.py

---
+++ pands-weekly-tasks/squareroot.py
@@ -0,0 +1,13 @@
+def sqrt():
+    num = float(input("Please enter a positive number higher than 0: "))
+    while num <= 0:
+        num = float(input("Please enter a positive number higher than 0: "))
+        if num > 0:
+            break
+    approx = 0.5 * num
+    better = 0.5 * (approx + num/approx)
+    while better != approx:
+        approx = better
+        better = 0.5 * (approx + num/approx)
+    print("The square root of", num, "is approx.", round(better, 2))
+sqrt()
