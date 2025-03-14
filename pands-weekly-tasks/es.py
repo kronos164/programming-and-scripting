fb573f247117f891cbe5bd2274de51d67bfb7f57
Cainã Oliveira <caina.boliveira@outlook.com>
2025-03-14 17:14:46 +0000

    Add script to count occurrences of 'e' in a specified file as the assingment 7

---
+++ pands-weekly-tasks/es.py
@@ -0,0 +1,16 @@
+import sys
+
+if len(sys.argv) > 1:
+    file = sys.argv[1]
+    directory = "./programming-and-scripting/pands-weekly-tasks/"
+    file = directory + file
+    print(file)
+    with open(file, "r") as f:
+        count = 0
+        for line in f:
+            for char in line:
+                if char == "e":
+                    count += 1
+        print(count)
+else:
+    print("Error: No file argument provided.")
